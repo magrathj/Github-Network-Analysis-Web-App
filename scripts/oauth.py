0e4b44a8f397d1a63ccfd981f16ee79dc1939051
magrathj <magrathj@tcd.ie>
2019-07-08 20:37:55 +0100

    added TODO github class to handle methods

--- scripts/oauth.py
+++ scripts/oauth.py
@@ -125,6 +125,12 @@ class User:
 		self.login = login
 
 
+## TODO create class Github to handle all Github methods
+class Github:
+	def __init__(self, client_id, client_secret):
+		self.client_id = client_id
+		self.client_secret = client_secret
+
 
 
 
